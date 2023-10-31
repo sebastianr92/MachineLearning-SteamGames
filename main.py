c86840c0f5f9a6a0d08c39af67ca1ac4ac2e3571
sebastianr92 <139284196+sebastianr92@users.noreply.github.com>
2023-10-31 12:54:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ FUNCIONES PARA ALIMENTAR LA API
 #librerías
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-#import pyarrow as pa
+import pyarrow as pa
 import pandas as pd
 import numpy as np
 #import scipy as sp
